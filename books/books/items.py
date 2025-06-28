84c949f19b725d5ec65b3a58f8812ec385bb21d4
Daniel Liu <danliu031505@gmail.com>
2025-06-28 17:11:27 -0500

    MongoDB implementation

--- books/books/items.py
+++ books/books/items.py
@@ -10,6 +10,7 @@ class BooksItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
 
+    _id = scrapy.Field()
     url = scrapy.Field()
     title = scrapy.Field()
     price = scrapy.Field()
--- books/books/spiders/book.py
+++ books/books/spiders/book.py
@@ -9,6 +9,14 @@ class BookSpider(scrapy.Spider):
     allowed_domains = ["books.toscrape.com"]
     start_urls = ["https://books.toscrape.com/"] #list of strings containing urls 
 
+    def start_requests(self):
+        for url in self.start_urls:
+            yield scrapy.Request(
+                url,
+                callback=self.parse,
+                errback=self.log_error
+            )
+
     def parse(self, response):
         #parsing the data from the css selectors
         for book in response.css("article.product_pod"):
@@ -21,4 +29,13 @@ class BookSpider(scrapy.Spider):
         next_page = response.css("li.next > a::attr(href)").get() #grabbing the href link that takes the user to the next page of books
         if next_page: #if another page exists
             next_page_url = response.urljoin(next_page) #turning the href into an absolute url
-            yield scrapy.Request(url=next_page_url, callback=self.parse) #creates a scrapy request on the next page 
+            self.logger.info(
+                f"Navigating to next page with URL {next_page_url}"
+            )
+            yield scrapy.Request(
+                url=next_page_url, 
+                callback=self.parse,
+                errback=self.log_error) #creates a scrapy request on the next page 
+    
+    def log_error(self, failure):
+        self.logger.error(repr(failure))
